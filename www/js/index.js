50f9b7852e215d0607fa4c80d8a4a05359379c94
programdeveloper <davidkhukhua@gmail.com>
2015-07-31 18:16:00 +0400

    changed color again

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,7 @@ var app = {
        console.log("program started");
         
        if (cordova.platformId == 'android') {
-            StatusBar.backgroundColorByHexString("#333");
+            StatusBar.backgroundColorByHexString("#fff");
         }
     }
 };
