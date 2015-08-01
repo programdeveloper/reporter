82ba4c628d2bf897ee2741f6ae64b375c75a373d
programdeveloper <davidkhukhua@gmail.com>
2015-08-01 19:19:32 +0400

    settings

--- www/js/index.js
+++ www/js/index.js
@@ -15,10 +15,26 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-       console.log("program started");
-        
-       if (cordova.platformId == 'android') {
-            StatusBar.backgroundColorByName("red");
+        if(!(localStorage.getItem("fullname") === null )){
+            $("#fullname").val(localStorage.getItem("fullname"));
+        }
+        if(!(localStorage.getItem("phone") === null )){
+            $("#phone").val(localStorage.getItem("phone"));
+        }
+        if(!(localStorage.getItem("email") === null)){
+            $("#email").val(localStorage.getItem("email"));                   
         }
     }
+
+    
 };
+function SaveInfo(){
+    var fullname = $("#fullname").val();
+    var phone  = $("#phone").val();
+    var email  = $("#email").val();
+
+    localStorage.setItem("fullname",fullname);
+    localStorage.setItem("phone",phone);
+    localStorage.setItem("email",email);
+     console.log(localStorage);
+}
